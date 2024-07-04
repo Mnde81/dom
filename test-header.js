6ccb59d89e0b76a489dd5aa7f860653a34079467
Mnde81 <minubis@gmail.com>
2024-07-04 12:37:51 +0300

    updated file

--- test-header.js
+++ test-header.js
@@ -2,9 +2,9 @@ const headerDOM = document.getElementsByTagName('header')[0];
 
 headerDOM.innerHTML =`
 <nav>
-    <a href="./">Home</a>
-    <a href="./about-us/">About-us</a>
-    <a href="./products/">Products</a>
-    <a href="./test-header/">Test-header</a>
-    <a href="./contacts/">Contact</a>
+    <a href="/">Home</a>
+    <a href="/about-us/">About-us</a>
+    <a href="/products/">Products</a>
+    <a href="/test-header/">Test-header</a>
+    <a href="/contacts/">Contact</a>
 </nav>`;
